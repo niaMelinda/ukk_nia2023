62f3f92c253565a499d820e1903b41efbcfec259
niaMelinda <110008000+niaMelinda@users.noreply.github.com>
2023-01-29 21:03:36 -0800

    Add files via upload

---
+++ PENGADUAN_MASYARAKAT.NIA/view/register.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+    
+  <div class="container">
+    <div class="row">
+      <div class="col-md-6 offset-md-3">
+        <h2 class="text-center text-dark mt-5">REGISTER</h2>
+        <div class="text-center mb-5 text-dark">PENGADUAN MASYRAKAT</div>
+        <div class="card my-5">
+
+          <form class="card-body cardbody-color p-lg-5">
+
+            <div class="text-center">
+              <img src="https://cdn.pixabay.com/photo/2016/03/31/19/56/avatar-1295397__340.png" class="img-fluid profile-image-pic img-thumbnail rounded-circle my-3"
+                width="200px" alt="profile">
+            </div>
+
+            <div class="mb-3">
+              <input type="text" class="form-control" id="NIK" aria-describedby="emailHelp"
+                placeholder="NIK">
+            </div>
+            <div class="mb-3">
+              <input type="date" class="form-control" id="TANGGAL LAHIR" aria-describedby="emailHelp"
+                placeholder="TANGGAL LAHIR">
+            </div>
+            <div class="mb-3">
+              <input type="NAMA LENGKAP" class="form-control" id="NAMA LENGKAP" placeholder="NAMA LENGKAP">
+            </div>
+            <div class="mb-3">
+              <input type="text" class="form-control" id="PASSWORD" aria-describedby="emailHelp"
+                placeholder="PASSWORD">
+            </div>
+            <div class="mb-3">
+              <input type="text" class="form-control" id="COMFIRM PASSWORD" aria-describedby="emailHelp"
+                placeholder="COMFIRM PASSWORD">
+            </div>
+            <div class="text-center"><button type="submit" class="btn btn-color px-5 mb-5 w-100">Register</button></div>
+            <div id="emailHelp" class="form-text text-center mb-5 text-dark">
+            are you the owner of this account? <a href="#" class="text-dark fw-bold"></a>
+            </div>
+          </form>
+        </div>
+
+      </div>
+    </div>
+  </div>
+
+<br>
+
+<div class="text-center">
+<br>
+
+<?php
+?>
+</form>
+
+</div>
+</div>
+</body>
+</html>
